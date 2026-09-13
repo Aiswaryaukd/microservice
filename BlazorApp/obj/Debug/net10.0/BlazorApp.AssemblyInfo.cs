148436f4b108462f7b05c048f72291800cc9513c
aiswarya <aiswarya@gmail.com>
2026-09-13 15:23:33 +0530

    post contyroller implemented

--- BlazorApp/obj/Debug/net10.0/BlazorApp.AssemblyInfo.cs
+++ BlazorApp/obj/Debug/net10.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88310680a13ff7b1c58771dea4f02c4f1c1a0bfb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
